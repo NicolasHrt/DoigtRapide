9ef10067577296a8cafb7862be92b1f796c79156
Nico <72673830+NicolasHrt@users.noreply.github.com>
2022-01-25 16:46:03 +0100

    Appel d'API qui fonctionne

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MainRapideComponent } from './main-rapide/main-rapide.component';
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainRapideComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
